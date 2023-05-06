f28edf58dbeb648c182184643f95fed51142687c
Mykola <mykola.zhuk@mail.utoronto.ca>
2023-05-06 16:42:04 -0400

    Added CORS to work with flask

--- backend/application/__init__.py
+++ backend/application/__init__.py
@@ -1,8 +1,13 @@
 from flask import Flask
 from pymongo import MongoClient
+<<<<<<< Updated upstream
 import cohere
+=======
+from flask_cors import CORS
+>>>>>>> Stashed changes
 
 app = Flask(__name__)
+CORS(app)
 app.config["SECRET_KEY"] = "ffa5538b7e176c8ddb4fe1fb39a2775dbe9e1643"
 
 cluster = "mongodb+srv://mykola:Qw121213@methacksprojectdb.s3vynya.mongodb.net/?retryWrites=true&w=majority"
